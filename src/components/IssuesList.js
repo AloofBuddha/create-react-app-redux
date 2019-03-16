490b792a3d31b66bbe8aaf545a8fa702fee7fa93
Benjamin Cohen <bac1087@gmail.com>
2019-03-15 23:27:10 -0400

    implemented sessionstorage

--- src/components/IssuesList.js
+++ src/components/IssuesList.js
@@ -5,27 +5,48 @@ import axios from 'axios';
 import './IssuesList.css';
 
 let IssuesList = ({ token, repo }) => {
-  let [issues, setIssues] = useState([]);
+  let [issues, setIssues] = useState(JSON.parse(sessionStorage.getItem("issues")) || []);
+  let [sort, setSort] = useState(sessionStorage.getItem("sort") || "created");
+  let [direction, setDirection] = useState(sessionStorage.getItem("direction") || "desc");
 
   useEffect(() => {
-      getIssues(token, repo)
-      .then(issues => setIssues(issues));
-    }, [token, repo])
+      getIssues(token, repo, sort, direction)
+      .then(issues => {
+        setIssues(issues);
+        sessionStorage.setItem('issues', JSON.stringify(issues));
+      });
+    }, [token, repo, sort, direction])
 
 
   return (
     <div className="issues-container">
       <ul style={{ listStyle: 'none' }}>
+        <li>
+          <span>Avatar</span>
+          <span>name</span>
+          <button onClick={() => {
+            setSort("created")
+            sessionStorage.setItem('sort', "created");
+            setDirection(direction === 'asc' ? 'desc' : 'asc') 
+            sessionStorage.setItem('direction', direction === 'asc' ? 'desc' : 'asc');         
+          }}>Created At ({direction})</button>
+          <button onClick={() => {
+            setSort("updated")
+            sessionStorage.setItem('sort', "updated");
+            setDirection(direction === 'asc' ? 'desc' : 'asc')
+            sessionStorage.setItem('direction', direction === 'asc' ? 'desc' : 'asc');        
+          }}>Updated At ({direction})</button>
+        </li>
         {
         issues.map(issue => (
             <li key={issue.id}>
             {
-                issue.assignee && 
-                <img src={issue.assignee.avatar_url} width="40" height="40" alt="assignee avatar url"/>
+              issue.assignee && 
+              <img src={issue.assignee.avatar_url} width="40" height="40" alt="assignee avatar url"/>
             }
-            <span>{issue.title}</span>
-            <span>{moment(issue.created_at).format("DD/MM/YYYY")}</span>
-            <span>{moment(issue.updated_at).fromNow()}</span>
+              <span>{issue.title}</span>
+              <span>{moment(issue.created_at).format("DD/MM/YYYY")}</span>
+              <span>{moment(issue.updated_at).fromNow()}</span>
             </li>
         ))
         }
@@ -34,13 +55,17 @@ let IssuesList = ({ token, repo }) => {
   );
 }
 
-async function getIssues(apiKey, repo) {
-  let issues = await axios.get(
+async function getIssues(apiKey, repo, sort, direction) {
+  let response = await axios.get(
     `https://api.github.com/repos/${repo.owner.login}/${repo.name}/issues`, {
     headers: { 'Authorization': `token ${apiKey}` },
+    params: { 
+      sort,
+      direction,
+    }
   })
 
-  return issues.data;
+  return response.data;
 }
 
 export default IssuesList;
--- src/components/RepoList.js
+++ src/components/RepoList.js
@@ -7,16 +7,27 @@ import IssuesList from './IssuesList'
 
 let RepoList = (props) => {
 
-    // let apiKey = prompt("What's your Github API key?");
-  // DEBUG ONLY!
-  let token = "05e2a3655f706f69f0cd4182b35954d3f939edbd";
-
-  let [repos, setRepos] = useState([]);
-  let [selectedRepo, setselectedRepo] = useState(null);
+  let [token, setToken] = useState(sessionStorage.getItem("token"));
+  let [repos, setRepos] = useState(JSON.parse(sessionStorage.getItem("repos")) || []);
+  let [selectedRepo, setSelectedRepo] = useState(JSON.parse(sessionStorage.getItem("selectedRepo")));
 
   useEffect(() => {
+    if (!token) {
+      let prompt = window.prompt || (() => false);
+      let promptToken = prompt("What's your Github API key?");
+      sessionStorage.setItem('token', promptToken);
+      setToken(promptToken);
+    }
+}, [])
+
+  useEffect(() => {
+    if (token) {
       getRepos(token)
-      .then(repos => setRepos(repos));
+      .then(repos => {
+        setRepos(repos)
+        sessionStorage.setItem('repos', JSON.stringify(repos));
+      });
+    }
   }, [token])
 
   return (
@@ -26,8 +37,10 @@ let RepoList = (props) => {
         {
           repos.map((repo) => (
             <li key={repo.id} 
-                onClick={(event) => setselectedRepo(repo)}
-            >
+                onClick={(event) => {
+                  setSelectedRepo(repo)
+                  sessionStorage.setItem('selectedRepo', JSON.stringify(repo));
+                }}>
               {repo.name}
             </li>
           ))
@@ -42,15 +55,15 @@ let RepoList = (props) => {
 }
 
 async function getRepos(apiKey) {
-  let repos = await axios.get("https://api.github.com/user/repos", {
+  let response = await axios.get("https://api.github.com/user/repos", {
     headers: { 'Authorization': `token ${apiKey}` },
     params: { 
       per_page: 10,
       sort: "created",
     }
   })
-
-  return repos.data;
+  
+  return response.data;
 }
 
 export default RepoList;
